8ceb0242db78b0ba70ae27cd91a43ed4e5b2a265
Vinícius <Vincius@DESKTOP-HLF9SUT>
2022-07-06 19:55:21 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i;
+			for (i = 1; i <= 100 / 2; i++)
+			{
+				Console.WriteLine(i * 2);
+			}
+			Console.ReadLine();
+		}
+	}
+}
